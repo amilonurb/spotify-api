fd9d6bbcc663cd60aa688c5cd507a212a7ed282e
Bruno Rodrigues Lima <brunolima.18@outlook.com>
2021-07-28 21:16:22 -0300

    feat: add Services dependency injection config

---
+++ SpotifyWebAPI.IoC/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace SpotifyWebAPI.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDependencies(this IServiceCollection services)
+        {
+            services.Scan(scan => scan.FromAssemblies(Assembly.Load("SpotifyWebAPI.Services"))
+                                      .AddClasses(c => c.Where(type => type.Name.EndsWith("Service")))
+                                      .AsImplementedInterfaces()
+                                      .WithScopedLifetime());
+
+            return services;
+        }
+    }
+}
