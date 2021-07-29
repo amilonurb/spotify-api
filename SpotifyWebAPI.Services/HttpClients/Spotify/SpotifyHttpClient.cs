55c42dfa30a4b4dbe6b3a6bc182fb305e4c8ec19
Bruno Rodrigues Lima <brunolima.18@outlook.com>
2021-07-28 21:16:54 -0300

    feat: add SpotifyHttpClient

---
+++ SpotifyWebAPI.Services/HttpClients/Spotify/SpotifyHttpClient.cs
@@ -0,0 +1,15 @@
+﻿using SpotifyWebAPI.Application.Services.Common;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SpotifyWebAPI.Services.HttpClients.Spotify
+{
+    public class SpotifyHttpClient : IHttpClient<SpotifyHttpClient>
+    {
+        private readonly HttpClient _httpClient;
+
+        public SpotifyHttpClient(HttpClient httpClient) => _httpClient = httpClient;
+
+        public async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request) => await _httpClient.SendAsync(request);
+    }
+}
