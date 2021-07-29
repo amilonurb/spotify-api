7b35a7c419ac92f71498c38a75fba05370b94508
Bruno Rodrigues Lima <brunolima.18@outlook.com>
2021-07-28 21:14:58 -0300

    feat: add Services project

--- SpotifyWebAPI/Startup.cs
+++ SpotifyWebAPI/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using SpotifyWebAPI.IoC;
 
 namespace SpotifyWebAPI
 {
@@ -19,10 +20,13 @@ namespace SpotifyWebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "SpotifyWebAPI", Version = "v1" });
             });
+
+            services.AddDependencies();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
