182fe4a14280e9215c75f4f016a23ff91dee0387
Bruno Rodrigues Lima <brunolima.18@outlook.com>
2021-07-28 21:15:47 -0300

    feat: add IHttpClient

---
+++ SpotifyWebAPI.Application/Services/Common/IHttpClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SpotifyWebAPI.Application.Services.Common
+{
+    public interface IHttpClient<T>
+    {
+        Task<HttpResponseMessage> SendAsync(HttpRequestMessage request);
+    }
+}
